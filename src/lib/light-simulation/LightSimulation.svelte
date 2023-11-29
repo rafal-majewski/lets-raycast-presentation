1552fd480e0be76d8e934c4c9709cc55adf86ff7
Rafał Majewski <goodheropl@gmail.com>
2023-11-29 23:39:05 +0100

    Light simulation

---
+++ src/lib/light-simulation/LightSimulation.svelte
@@ -0,0 +1,17 @@
+<script lang="ts" strictEvents>
+	import Article from "$lib/utils/Article.svelte";
+</script>
+
+<Article>
+	<iframe
+		allowfullscreen
+		frameborder="0"
+		src="https://www.loom.com/embed/65ab94763d8c4d4d9ac031fac9154500?sid=6c64ee4b-8131-46f1-857d-b1efb48b19e3"
+		style:height="100%"
+		style:width="100%"
+		title="Light Simulation"
+	>
+	</iframe>
+	<br /><br /><br /><br /><br /><br />
+	<a href="raycasting-example">Go to a raycasting example</a>
+</Article>
---
+++ src/routes/light-simulation/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts" strictEvents>
+	import LightSimulation from "$lib/light-simulation/LightSimulation.svelte";
+</script>
+
+<LightSimulation />
